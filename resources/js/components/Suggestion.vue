7d8b11ac32aee8d32348eb193363dc9353c819fc
Ruizlouka <86917525+Ruizlouka@users.noreply.github.com>
2022-02-23 10:45:30 +0100

    rajout fonction recherche

--- resources/js/components/Suggestion.vue
+++ resources/js/components/Suggestion.vue
@@ -412,7 +412,23 @@ export default {
 
     )
   },
+   Recherche() {
+      document.getElementById('recherche').addEventListener('keyup', function(e) {
+  var recherche = this.value.toLowerCase();
+  var documents = document.querySelectorAll('.document');
+ 
+  Array.prototype.forEach.call(documents, function(document) {
+    // On a bien trouvé les termes de recherche.
+    if (document.innerHTML.toLowerCase().indexOf(recherche) > -1) {
+      document.style.display = 'block';
+    } else {
+      document.style.display = 'none';
+    }
+  });
+});
+    },
   }
-}
+  }
+
 
 </script>
