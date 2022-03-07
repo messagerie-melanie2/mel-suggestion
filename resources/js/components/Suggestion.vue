fcca694aa26da93c7a1c92757b2dc51c77912fb4
Ruizlouka <86917525+Ruizlouka@users.noreply.github.com>
2022-03-07 09:31:23 +0100

    verification des function js

--- resources/js/components/Suggestion.vue
+++ resources/js/components/Suggestion.vue
@@ -1,296 +1,3 @@
-<template>
-
-<div v-if="Role === 'moderateur'">
-  <div>
-      <b-row>
-        <b-col cols="6" md="4"> </b-col>
-        <b-col cols="12" md="8"><h1>Suggestion</h1> </b-col>
-      </b-row>
-      <b-col cols="6" md="4">
-        <b-button variant="primary" @click="fetchAllsuggestion(today)"
-          >New</b-button
-        >
-        <b-button variant="primary" @click="fetchAllsuggestion(top)"
-          >Top</b-button
-        ></b-col
-      >
-       <b-button variant="primary" @click="Generatefilr() "
-          >Top</b-button>
-        
-      
-      <b-col cols="2" md="4">
-        <input
-          type="Search"
-          id="Search"
-          placeholder="Search une suggestion"
-          class="
-            appearance-none
-            border border-gray-400 border-b
-            block
-            pl-8
-            pr-6
-            py-2
-            w-full
-            bg-white
-            text-sm
-            placeholder-gray-400
-            rounded-lg
-            text-gray-700
-            focus:bg-white
-            focus:placeholder-gray-600
-            focus:text-gray-700
-            focus:outline-none
-          "
-          @keyup="ser"
-          v-on:change="Search"
-          v-model="search"
-        />
-      </b-col>
-      <b-col cols="2" md="4"
-        ><b-button v-b-modal.modal-prevent-closing class="pull-right"
-          >Creer Suggestion</b-button
-        >
-      </b-col>
-     
-
-      <b-modal
-        id="modal-prevent-closing"
-        ref="modal"
-        title="Faire une Suggestion"
-      >
-        <form ref="form" @submit.stop.prevent="handleSubmit">
-          <b-form-group
-            label="Titre"
-            label-for="name-input"
-            invalid-feedback="Name is required"
-          >
-            <b-form-input
-              id="name-input"
-              v-model="title"
-              required
-            ></b-form-input>
-          </b-form-group>
-          <b-form-group
-            label="Description"
-            label-for="name-input"
-            invalid-feedback="Name is required"
-          >
-            <b-form-textarea
-              id="textarea"
-              v-model="description"
-              placeholder="Une description"
-              rows="3"
-              max-rows="6"
-            ></b-form-textarea>
-          </b-form-group>
-          <b-btn @click="AddSuggestion">Ajouté</b-btn>
-        </form>
-      </b-modal>
-    </div>
-    <h2>Liste des Suggestion à valider</h2>
-    <b-card-group deck v-for="suggestion in ListSuggestion" :key="suggestion.id">
-      <b-card v-if="(suggestion.etat = '1')">
-        <b-card-text> Titre {{ suggestion.Title }} </b-card-text>
-        <b-card-text>
-          {{ suggestion.Description }}
-        </b-card-text>
-        <b-card-text> nb votes:{{ suggestion.number_votes }} </b-card-text>
-        <div v-if="(suggestion.etat = '1')">
-          <b-button
-            variant="danger"
-            @click="Updateetatsuggestion(5, sugesstion.id)"
-            >Pas bon</b-button
-          >
-          <b-button
-            variant="success"
-            v-b-modal.modal-1
-            @click="Updateetatsuggestion(2, sugesstion.id)"
-            >à Modifier</b-button
-          >
-          <b-button
-            variant="outline-primary"
-            @click="Updateetatsuggestion(3, sugesstion.id)"
-            >Valide</b-button
-          >
-          <b-button @click="Updateetatsuggestion(4, sugesstion.id)">Vérouiller</b-button> 
-        </div>
-        <div v-else>
-          <b-card-text> {{ suggestion.Title }} </b-card-text>
-          <b-card-text>
-            {{ suggestion.Description }}
-          </b-card-text>
-          <b-card-text> {{ suggestion.number_votes }} </b-card-text>
-         
-        </div>
-      </b-card>
-    </b-card-group>
-     <b-btn @click="Vote(suggestion.id)">Vote</b-btn>
-          <b-btn @click="DeleteVote(suggestion.id)">Enlever vote</b-btn>
-  </div>
-<div v-else>
-     <b-row>
-        <b-col cols="6" md="4"> </b-col>
-        <b-col cols="12" md="8"><h1>Suggestion</h1> </b-col>
-      </b-row>
-      <b-col cols="6" md="4">
-        <b-button variant="primary" @click="fetchAllsuggestion(init)"
-          >New</b-button
-        >
-        <b-button variant="primary" @click="fetchAllsuggestion(top)"
-          >Top</b-button
-        ></b-col
-      >
-     
-        <input
-          type="Search"
-          id="Search"
-          placeholder="Search une suggestion"
-          class="
-            appearance-none
-            border border-gray-400 border-b
-            block
-            pl-8
-            pr-6
-            py-2
-            w-full
-            bg-white
-            text-sm
-            placeholder-gray-400
-            rounded-lg
-            text-gray-700
-            focus:bg-white
-            focus:placeholder-gray-600
-            focus:text-gray-700
-            focus:outline-none
-          "
-          @keyup="ser"
-          v-on:change="Search"
-          v-model="search"
-        />
-         <b-col cols="2" md="4"
-        ><b-button v-b-modal.modal-prevent-closing class="pull-right"
-          >Creer Suggestion</b-button
-        >
-      </b-col>
-
-      <b-modal
-        id="modal-prevent-closing"
-        ref="modal"
-        title="Faire une Suggestion"
-      >
-        <form ref="form" @submit.stop.prevent="handleSubmit">
-          <b-form-group
-            label="Titre"
-            label-for="name-input"
-            invalid-feedback="Name is required"
-          >
-            <b-form-input
-              id="name-input"
-              v-model="newSuggestion.title"
-              required
-            ></b-form-input>
-          </b-form-group>
-          <b-form-group
-            label="Description"
-            label-for="name-input"
-            invalid-feedback="Name is required"
-          >
-            <b-form-textarea
-              id="textarea"
-              v-model="description"
-              placeholder="Une description"
-              rows="3"
-              max-rows="6"
-            ></b-form-textarea>
-          </b-form-group>
-          <b-btn @click="UpdateSuggestion">Ajouté</b-btn>
-        </form>
-      </b-modal>
-      <b-modal
-        id="modal_modifier"
-        ref="modal"
-        title="Modifier la Suggestion"
-      >
-        <form ref="form" @submit.stop.prevent="handleSubmit">
-          <b-form-group
-            label="Titre"
-            label-for="name-input"
-            invalid-feedback="Name is required"
-          >
-            <b-form-input
-              id="name-input"
-              v-model="SuggestionDetail.title"
-              required
-            ></b-form-input>
-          </b-form-group>
-          <b-form-group
-            label="Description"
-            label-for="name-input"
-            invalid-feedback="Name is required"
-          >
-            <b-form-textarea
-              id="textarea"
-              v-model="SuggestionDetail.description"
-              placeholder="Une description"
-              rows="3"
-              max-rows="6"
-            ></b-form-textarea>
-          </b-form-group>
-          <b-btn @click="M">Modifier</b-btn>
-        </form>
-      </b-modal>
-      <h2>Liste de mes suggestion<h2>
-        <div id="documents" >
-    <b-card-group deck v-for="suggestion in ListSuggestion" :key="suggestion.id">
-      <div v-if="suggestion.appartien='oui'">
-      <b-card >
-        <b-card-text class="document"> Titre {{ suggestion.title }} </b-card-text>
-        <b-card-text>
-          {{ suggestion.description}}
-        </b-card-text>
-
-        <b-card-text>{{ suggestion.number_votes}} </b-card-text>
-        <div v-if="suggestion.state=='1'">
-          <b-button  
-            variant="success"
-            v-b-modal.modal-1
-            @click="Updateetatsuggestion(2, sugesstion.id)"
-            >à Modifier</b-button
-          >
-            <b-button v-if="suggestion.state=='1' ||suggestion.appartien=='oui' "
-            variant="danger"
-            @click="Updateetatsuggestion(suprimer, sugesstion.id)"
-            >suprimer</b-button
-
-          >
-        </div>
-        <div v-else-if="suggestion.state='3'">
-           <b-button  
-            variant="success"
-          v-b-modal.modal-modif
-            @click=" Updateeta(modifier,suggestion.id)"
-            >Modifier</b-button
-          >
-           <b-button  
-            variant="success"
-            v-b-modal.modal-1
-            @click="Updateetatsuggestion(5, sugesstion.id)"
-            >suprimer</b-button
-          >
-
-        </div>
-        
-
-      </b-card>
-      
-    </b-card-group>
-     
-</div>
-
-    
-</template>
- 
-
 <script>
 import BootsrapVue from "bootstrap-vue";
 Vue.use(BootsrapVue);
@@ -312,7 +19,13 @@ export default {
      
     };
   },
+  /**
+  Ici j'initialise
+   */
   created() {
+    /**Je récupère le rôle de l'utilisateur envoyer par le php
+     * et j'initialise la listes des suggestions au lancement de l'application
+     */
     Role=JSON($user)
     fetchAllsuggestion(init);
   },
@@ -327,6 +40,10 @@ export default {
       date: this.newSuggestion.date,
     });
   },
+  /**
+   * Cette fonction me permet de récupérer toutes les suggestions de la base de données
+   * Ci la Liste des suggestion est vide , alors elle vas récupérer
+   */
   fetchAllsuggestion(Place) {
     if(this.ListSuggestion.length()=0){
       let urlfinal = converturl ("/RecoverySuggestion");
@@ -334,10 +51,14 @@ export default {
     axios
       .get(urlfinal)
       .response((response) => (this.ListSuggestion = response.data));
-    }else{
+ /**
+  * Ici selon l'endroit ou j'appel la function ca envoie un argument avec une valeur qui permet de faire un trie
+  * Le premier étant pour avoir la listes de celui qui à le plus grand nombre de votes 
+  * Le deuxième étant pour avoir les dernières sugestion qui sont sortir
+  */
      if (Place === "init") {
       this.ListSuggestion.sort((a, b) => a.number_votes - b.number_votes);
-    } else {
+       } else {
       this.ListSuggestion.sort((a, b) => a.start_date - b.start_date);a
     }
 
@@ -345,21 +66,21 @@ export default {
 
 
   },
- 
+ /**La function me permet de modifier l'etat de la suggedtions
+  * Ci la le paramètre envoyer est supprimer alors ca déclanche la supression de la suggestion
+  * Et sinon elle appel la function de changement d'état en prennant la valeur de l'état et l'id de la sugestion
+  */
 Updateetatsuggestion(type, id) {
     
       if(type="5"){
-         urlfinal = converturl ( "/Deletesuggestion");
-      
-      axios
-      .delete(urlfinal + id);
-      let urlfinal = converturl ("/DeleteVote");
-      }else{
-        let urlfinal = converturl ( "/UpdateState");
-     axios.
-      put(urlfinal, {etat: type,id_suggestion: id});
+        urlfinal = converturl ( "/Deletesuggestion");
+        axios.delete(urlfinal + id);
+        let urlfinal = converturl ("/DeleteVote");
+           }else{
+            let urlfinal = converturl ( "/UpdateState");
+            axios.put(urlfinal, {etat: type,id_suggestion: id});
 
-      }
+    }
 
    
   },
@@ -395,9 +116,7 @@ Updateetatsuggestion(type, id) {
   Generatefilr(){
    
      let urlfinal = converturl ("/GenerationCSV");
-      axios.get(urlfinal
-
-    )
+      axios.get(urlfinal)
   },
    Recherche() {
       document.getElementById('recherche').addEventListener('keyup', function(e) {
