de4501eb96ce01aec3988f7e78174ed64ef91e79
AGoubs <arnaud@goubier.fr>
2022-06-08 10:43:47 +0200

    Push library

--- mel-suggestion-front/tailwind.config.js
+++ mel-suggestion-front/tailwind.config.js
@@ -3,13 +3,6 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  purge: [
-    "./pages/**/*.vue", 
-    "./components/**/*.vue", 
-    "./plugins/**/*.vue",
-    "./static/**/*.vue",
-    "./store/**/*.vue"
-  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
