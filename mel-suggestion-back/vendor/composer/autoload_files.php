45b9b26b7f607c48af95601847f708e0e6b009de
AGoubs <arnaud@goubier.fr>
2022-07-20 13:54:54 +0200

    Add OpenID-Connect-PHP library

--- mel-suggestion-back/vendor/composer/autoload_files.php
+++ mel-suggestion-back/vendor/composer/autoload_files.php
@@ -34,5 +34,6 @@ return array(
     '9cdd7b9056abc3081735233ba9dd9c7f' => $vendorDir . '/facade/flare-client-php/src/helpers.php',
     '6124b4c8570aa390c21fafd04a26c69f' => $vendorDir . '/myclabs/deep-copy/src/DeepCopy/deep_copy.php',
     'b6b991a57620e2fb6b2f66f03fe9ddc2' => $vendorDir . '/symfony/string/Resources/functions.php',
+    'decc78cc4436b1292c6c0d151b19445c' => $vendorDir . '/phpseclib/phpseclib/phpseclib/bootstrap.php',
     'ed962a97bd972bc82007176b647d4e36' => $vendorDir . '/facade/ignition/src/helpers.php',
 );
