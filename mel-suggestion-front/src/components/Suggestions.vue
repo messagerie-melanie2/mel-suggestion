1a3c9e8eb7f416cd7713173126c51c6745bfbd19
user <user@bnum.bnum.mte.com>
2024-05-30 10:16:37 +0200

    Modification déclenchement du message indiquant le chargement des données si l'utilisateur commence une recherche et que les données n'ont pas été encore chargées.

--- mel-suggestion-front/src/components/Suggestions.vue
+++ mel-suggestion-front/src/components/Suggestions.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative overflow-x-auto">
-    <div v-if="isLoading" class="loading-message absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2">
+    <div v-if="isLoading && isSearching" class="loading-message absolute -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2">
       <svg aria-hidden="true" class="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
         <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
@@ -50,7 +50,6 @@ import Suggestion from "./Suggestion";
 import CreateSuggestion from "./CreateSuggestion";
 import unaccent from "unaccent";
 import { mapGetters } from "vuex";
-// import { synonymsArray } from "@/dictionary"; // Importer le dictionnaire et les synonymes
 
 /**
  * Normalise une chaîne de caractères en la convertissant en minuscules,
@@ -140,7 +139,8 @@ export default {
       validateOnly: false,
       refusedSuggestion: false,
       synonymsArray: [],
-      isLoading: true // Variable d'état pour le chargement
+      isLoading: true, // Variable d'état pour le chargement
+      isSearching: false // Variable d'état pour savoir si l'utilisateur a commencé une recherche
     };
   },
   mounted() {
@@ -169,9 +169,7 @@ export default {
       this.isLoading = true;
       try {
         const response = await fetch('https://rcube.preprod.m2.e2.rie.gouv.fr/bureau/Synonymes.json');
-         
         this.synonymsArray = await response.json();
-        console.log(this.synonymArray)
       } catch (error) {
         console.error('Erreur lors du chargement des synonymes:', error);
       } finally {
@@ -184,6 +182,7 @@ export default {
       this.refusedSuggestion = r;
     },
     searchValue(s) {
+      this.isSearching = true; // Indique que l'utilisateur a commencé une recherche
       const searchNormalized = normalizeString(s);
       this.search = searchNormalized;
     },
@@ -192,6 +191,7 @@ export default {
     },
     resetSearch() {
       this.search = "";
+      this.isSearching = false; // Réinitialiser l'état de recherche lorsque la recherche est réinitialisée
       this.$root.$emit('reset-search-text');
     },
 
@@ -297,7 +297,6 @@ export default {
     searchWithSynonyms(searchWord) {
       if (this.synonymsArray[searchWord])  {
           return [searchWord, ...(Array.isArray(this.synonymsArray[searchWord]) ? this.synonymsArray[searchWord] : [])];
-         
       }
       return [];
     },
