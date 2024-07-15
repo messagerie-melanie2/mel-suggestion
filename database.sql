69cbec771724b949596cfd0c2ba19a0d6ee225d5
AGoubs <39366401+AGoubs@users.noreply.github.com>
2024-07-15 16:34:10 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-create table "suggestions" ("id" bigserial primary key not null, "title" varchar(255) not null, "description" text not null, "user_email" varchar(255), "user_lastname" varchar(255) null, "user_firstname" varchar(255) null, "state" varchar(255) not null, "instance" varchar(255) not null, "created_at" timestamp(0) without time zone null, "updated_at" timestamp(0) without time zone null);
+create table "suggestions" ("id" bigserial primary key not null, "title" varchar(255) not null, "description" text not null, "user_email" varchar(255), "user_lastname" varchar(255) null, "user_firstname" varchar(255) null,"comment" text, "state" varchar(255) not null, "instance" varchar(255) not null, "created_at" timestamp(0) without time zone null, "updated_at" timestamp(0) without time zone null);
 
 create index "suggestions_instance_index" on "suggestions" ("instance");
 
