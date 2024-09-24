f4adb64aa400bee4fae0a8eeada1a58a51f0c737
agoubier <agoubier>
2024-09-24 14:06:38 +0200

    fix : Ajout de REDIS_PORT

--- mel-suggestion-back/config/database.php
+++ mel-suggestion-back/config/database.php
@@ -140,7 +140,7 @@ return [
                     return [
                         'host' => $host,
                         'password' => env('REDIS_PASSWORD', null),
-                        'port' => env('REDIS_POST', 6379),
+                        'port' => env('REDIS_PORT', 6379),
                         'database' => env('REDIS_DB', '0'),
                     ];
                 }, explode(',', env('REDIS_HOST', '127.0.0.1'))),
